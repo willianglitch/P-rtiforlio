4bed538b9e74d18176295ba4e9d7470cdaec5314
Willian da silva soares <90284411+willianglitch@users.noreply.github.com>
2022-06-25 19:02:32 -0300

    commit 11
    trabalhando na atualização!! , parte dos prójetos e passando para o js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,5 @@
 // pegar o atributo da imagem 
 // preciso que sejá mudado para outro src quando o mouse passar em cima 
+
+const minhaFoto = document.querySelector("#minhaFoto")
+console.log(minhaFoto)
